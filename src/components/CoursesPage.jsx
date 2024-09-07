aee3095405c0c05b8bee48ff07edee2f1bb73c17
krishkh <krishkhanna00@gmail.com>
2024-09-08 00:49:33 +0530

    made sidebar better

--- src/components/CoursesPage.jsx
+++ src/components/CoursesPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Sidebar from "./Sidebar";
 
 const coursesData = [
   {
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,33 +1,32 @@
-function Sidebar() {
-    return (
-        <aside className="w-52 bg-gray-100 shadow-xl shadow-gray-300 border h-auto p-6">
-          <ul className="space-y-6">
-            <li><a href="/dashboard" className="hover:bg-gray-200 p-2 rounded-md cursor-pointer">
-              Dashboard
-            </a></li>
-            <li><a href="/profile" className="hover:bg-gray-200 p-2 rounded-md cursor-pointer">
-              Profile
-            </a></li>
-            <li><a className="hover:bg-gray-200 p-2 rounded-md cursor-pointer">
-              Community
-            </a></li>
-            <li><a className="hover:bg-gray-200 p-2 rounded-md cursor-pointer">
-              Courses
-            </a></li>
-            <li><a href="/tests" className="hover:bg-gray-200 p-2 rounded-md cursor-pointer">
-              Assessments
-            </a></li>
-            <li><a className="hover:bg-gray-200 p-2 rounded-md cursor-pointer">
-              Interviews
-            </a></li>
-            <li><a className="hover:bg-gray-200 p-2 rounded-md cursor-pointer">
-              Settings
-            </a></li>
-            <li><a href="/" className="hover:bg-gray-200 p-2 rounded-md cursor-pointer text-red-500">
-              Log Out
-            </a></li>
-          </ul>
-        </aside>
-    )
-}
+import React from "react";
+import { Link } from "react-router-dom";
+const navLinks = [
+  { name: "Dashboard", link: "/dashboard" },
+  { name: "Profile", link: "/profile" },
+  { name: "Community", link: "/community" },
+  { name: "Courses", link: "/courses" },
+  { name: "Assessments", link: "/tests" },
+  { name: "Interviews", link: "/interviews" },
+  { name: "Settings", link: "/settings" },
+  { name: "Logout", link: "/" },
+];
+
+const Sidebar = () => {
+  return (
+    <aside className="w-52 bg-gray-100 shadow-xl shadow-gray-300 border h-auto p-6">
+      <ul className="space-y-6">
+        {navLinks.map((link, index) => (
+          <li key={index}>
+            <Link
+              to={link.link}
+              className="hover:bg-gray-200 p-2 rounded-md cursor-pointer"
+            >
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
 export default Sidebar;
